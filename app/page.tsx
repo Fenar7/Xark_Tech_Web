fa7997633812bdb68611a35d33c47a4841d92289
Fenar7 <fenarkhan@gmail.com>
2026-02-25 00:11:43 +0530

    core capabilities section ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero/Hero';
 import AboutSection from './components/AboutSection/AboutSection';
+import CoreCapabilitiesSection from './components/CoreCapabilitiesSection/CoreCapabilitiesSection';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <AboutSection />
+      <CoreCapabilitiesSection />
     </main>
   );
 }
