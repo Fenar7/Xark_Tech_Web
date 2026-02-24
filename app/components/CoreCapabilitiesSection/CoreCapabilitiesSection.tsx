51bd0e72ca294d449c1fa9659085ea2932d1c376
Fenar7 <fenarkhan@gmail.com>
2026-02-24 23:57:46 +0530

    core capabilities section

---
+++ app/components/CoreCapabilitiesSection/CoreCapabilitiesSection.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CoreCapabilitiesSection = () => {
+  return (
+    <div className='capabilities-section-container flex items-center justify-center'>
+<div className="capabilties-section content">
+    <div className="title-section">
+        #pill
+        #title
+        #subtext
+    </div>
+
+    
+</div>
+    </div>
+  )
+}
+
+export default CoreCapabilitiesSection
