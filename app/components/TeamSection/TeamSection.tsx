194f9245d90b9c426cddee0c198e4a763f42d536
Fenar7 <fenarkhan@gmail.com>
2026-02-26 12:29:52 +0530

    new sec abt initsdfsdf

---
+++ app/components/TeamSection/TeamSection.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const TeamSection = () => {
+  return (
+    <div className='team-section-container-main flex items-center justfiy-center'>
+<div className="team-section container">
+    #titlegoes here
+    <div className="team-cards-container">
+        <div className="team-item-card">
+            <img src="" alt="" />
+
+            <div className="content-container">
+                <div className="left-section">
+
+                </div>
+
+                <div className="right-section">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default TeamSection
